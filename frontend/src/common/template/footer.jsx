5035f9a19c7e11813c5053ec04d58607dcb6b679
Diego Ronan Kulian <dkulian@veepee.com>
2023-09-29 18:31:54 +0200

    creating menu and footer pages

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+         POC React Advanced Concepts.
+        </strong>
+    </footer>
+)
