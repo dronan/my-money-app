9b4b26ea745e73a671f5b4a2011db38035517c24
Diego Ronan Kulian <dkulian@veepee.com>
2023-10-02 14:02:36 +0200

    line components

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -3,16 +3,18 @@ import React, { Component } from "react";
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
 import ValueBox from "../common/widget/valueBox";
-
+import Row from "../common/layout/row";
 class Dashboard extends Component {
     render() {
         return (
             <div>
                 <ContentHeader title="Dashboard" subtitle="V1.0" />
                 <Content>
-                    <ValueBox cols="12 4" color="green" icon="bank" value="€ 10" text="Total Credits" />
-                    <ValueBox cols="12 4" color="red" icon="credit-card" value="€ 10" text="Total Debts" />
-                    <ValueBox cols="12 4" color="blue" icon="money" value="€ 0" text="Consolidated" />
+                    <Row>
+                        <ValueBox cols="12 4" color="green" icon="bank" value="€ 10" text="Total Credits" />
+                        <ValueBox cols="12 4" color="red" icon="credit-card" value="€ 10" text="Total Debts" />
+                        <ValueBox cols="12 4" color="blue" icon="money" value="€ 0" text="Consolidated" />
+                    </Row>
                 </Content>
             </div>
         );
