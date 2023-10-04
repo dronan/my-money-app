c2280b1eb5c80dfc6574ec0227908b4b14907a84
Diego Ronan Kulian <dkulian@veepee.com>
2023-10-04 11:49:29 +0200

    bug fix

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -14,11 +14,11 @@ import Summary from "./summary";
 class BillingCycleForm extends Component {
 
   calculateSummary() {
-    const sum = (t, v) => t + v;
+    const sum = (t, v) => t + v
     return {
-      sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
-      sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
-    };
+        sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+        sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+    }
   }
 
   render() {
@@ -31,14 +31,14 @@ class BillingCycleForm extends Component {
       <form role="form" onSubmit={handleSubmit}>
         <div className="box-body">
             <Field name="name" component={labelAndInput} readOnly={readOnly}
-              label="Name" cols="12 4" placeholder="Enter the name"
+              label="Name" cols="12 4" placeholder="Enter the name" initialValue=""
             />
             <Field name="month" component={labelAndInput}  readOnly={readOnly} type="number"
-              label="Month" cols="12 4" placeholder="Enter the month"
+              label="Month" cols="12 4" placeholder="Enter the month" initialValue=""
               parse={parseNumber} format={formatNumber}
             />
             <Field name="year" component={labelAndInput}  readOnly={readOnly} type="number"
-              label="Year" cols="12 4" placeholder="Enter the year"
+              label="Year" cols="12 4" placeholder="Enter the year" initialValue=""
               parse={parseNumber} format={formatNumber}
             />
             <Summary credit={sumOfCredits} debt={sumOfDebts} />
@@ -58,7 +58,7 @@ class BillingCycleForm extends Component {
   }
 }
 
-BillingCycleForm = reduxForm({ form: "billingCycleForm" })(BillingCycleForm);
+BillingCycleForm = reduxForm({ form: "billingCycleForm", destroyOnUnmount: false })(BillingCycleForm);
 const selector = formValueSelector("billingCycleForm");
 const mapStateToProps = state => ({
   credits: selector(state, "credits"),
